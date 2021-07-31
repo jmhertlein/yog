59ea3a6d6fd4371535f697d9b68182c964a7f105
Josh Hertlein <jmhertlein@gmail.com>
2021-07-30 20:23:01 -0500

    clean up after deploy

--- deploy.bash
+++ deploy.bash
@@ -4,5 +4,10 @@ set -x
 set -e
 
 rm -r ./dist/ || true
+rm -r ./build/ || true
 pipenv run python3 setup.py sdist bdist_wheel
 pipenv run twine upload dist/*
+
+rm -r ./build/ || true
+rm -r ./dist/ || true
+rm -r yog.egg-info || true
