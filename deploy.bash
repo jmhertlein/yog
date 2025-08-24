077159381ddb3fafd07bc8156221fe618a5e0546
Josh Hertlein <jmhertlein@gmail.com>
2025-08-23 19:50:05 -0500

    we uv now

--- deploy.bash
+++ deploy.bash
@@ -6,9 +6,8 @@ set -e
 rm -r ./dist/ || true
 rm -r ./build/ || true
 #pipenv run python3 setup.py sdist bdist_wheel
-poetry install --with dev
-poetry build
-poetry publish
+uv build
+UV_PUBLISH_TOKEN="$(cat "$HOME/.uv-publish-token")" uv publish
 
 rm -r ./build/ || true
 rm -r ./dist/ || true
