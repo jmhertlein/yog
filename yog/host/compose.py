db7a15987f37239978201afe2f596d496ab4f271
Josh Hertlein <jmhertlein@gmail.com>
2025-09-30 13:53:06 -0500

    first cut of compose support

--- yog/host/compose.py
+++ yog/host/compose.py
@@ -3,15 +3,19 @@ import os.path
 import subprocess
 
 from yog.host.necronomicon import Necronomicon
+from yog.host.utils import get_path_for_file
 
 log = logging.getLogger(__name__)
 
 
 def apply_compose_section(host: str, n: Necronomicon, root_dir: str):
     log.info(f"[{host}][compose] up")
+    env = os.environ.copy()
+    env['DOCKER_HOST'] = f"ssh://{host}"
     subprocess.check_call([
         "docker",
         "compose",
-        "-f", os.path.join(root_dir, n.compose.compose_file_path),
+        "-f", get_path_for_file(n.compose.compose_file_path, root_dir),
         "up",
-    ], env={"DOCKER_HOST": f"ssh://{host}"})
+        "--detach",
+    ], env=env)
